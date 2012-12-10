235dfc4226760767b9d477ac2e89f835bf5b40de
jacob gardner <jacob.v.gardner@gmail.com>
2012-12-09 23:14:40 -0600

    Add turn tags

--- src/timeline.py
+++ src/timeline.py
@@ -19,6 +19,14 @@ class TrackObject(object):
 
 
 class Tag(object):
+    '''
+    Timeline tag object.
+
+    :param data: Data attached to the tag
+
+    :param position: Position on the timeline this tag would be found at.
+    :type position: float
+    '''
     def __init__(self, data, position):
         self.data = data
         self.position = position
@@ -29,33 +37,45 @@ class Timeline(object):
     Timeline class is responsible for controlling all the visible objects
 
     :param event_dispatcher: The event dispatcher for catching/responding and sending events to the rest of the system
-    :type event_dispatcher: eventdispatcher.EventDispatcher
+    :type event_dispatcher: EventDispatcher
     '''
     tracks = []
     starts = []
     ends = []
-
+    visible_tracks = []
     tags = {}
+    insert_pos = 0
+    turn_number = 1
+    ed = None
+
+    start_idx = 0
+    end_idx = 0
 
     def __init__(self, event_dispatcher):
         self.ed = event_dispatcher
-        self.last_time_input
-        self.insert_pos = 0
+
+    def determine_visible(self, time):
+        pass
 
     def add(self, obj, start_time=None):
         '''
         This method adds an object to the timeline to display.
 
         :param obj: The object to be controlled by the timeline
-        :type obj: Subclass of TrackObject
+        :type obj: TrackObject
 
         :param start_time: The timeline time slot that this object's life begins at.  Defaults to adding right after the last animation has completed.
         :type start_time: float
-
-
         '''
         assert issubclass(obj.__class__, TrackObject)
         self.tracks += [obj]
+
+    def add_turn(self):
+        '''
+        Inserts a turn tag to the timeline, automatically using the last position and incrementing the turn number.
+        '''
+        self.insert_tag('turn', self.turn_number)
+        self.turn_number += 1
 
     def insert_tag(self, tag_name, data=None, position=None):
         '''
@@ -64,7 +84,7 @@ class Timeline(object):
         :param tag_name: The name of the tag
         :type tag_name: string
 
-        :param data: Data to attach to the tag
+        :param data: Data to attach to the tag.  This can be any data that makes sense with the tag.  For example a 'turn' tag should probably have the turn number encoded with it.
 
         :param position: Timeline time slot of where the tag is inserted.  Defaults to right after the last animation has completed.
         :type position: float
